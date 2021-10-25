a9d016f804b361914a673b3625ae862834372b8d
Joe Alves <joseph.michael.alves@gmail.com>
2021-10-25 10:57:29 -0400

    push

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 
 const names = ["john", "mary", "sue", "link", "penny", "henry", "nugget", "max"];
 
+// Session
 const tagsISentOut = {};
 
 // When someone makes a request to a server,
@@ -15,7 +16,13 @@ app.get("/getLogin", (req, res) => {
             requestCount: tagsISentOut[previouslyAssignedNameTag]
         });
     } else {
-        const nameTag = names[Math.floor(Math.random() * 4)] + Date.now()
+        // Tokens.
+        const password = req.query.password;
+        if (password !== "bubblebop") {
+            res.status(401).send("Password incorrect");
+            return;
+        }
+        const nameTag = names[Math.floor(Math.random() * names.length)] + Date.now()
         tagsISentOut[nameTag] = 0;
         res.send({
             nameTag,
